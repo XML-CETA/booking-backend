c6e9da829362c2a6c858ce6696ff7635d49557a2
Luka Licina <licina.luka@gmail.com>
2023-05-14 23:23:02 +0200

    All WORKING FINALLY

--- accommodation_service/main.go
+++ accommodation_service/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"booking-backend/accommodation_service/startup"
 	cfg "booking-backend/accommodation_service/startup/config"
+	"log"
 )
 
 func main() {
 	config := cfg.NewConfig()
 	server := startup.NewServer(config)
+	log.Print(config)
 	server.Start()
 }
